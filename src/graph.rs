1050bf803533f8779896a7f531de5e3f02a91863
Edgar Lee <edgarhinshunlee@gmail.com>
2025-03-26 16:44:24 -0400

    Make file clonable

--- src/graph.rs
+++ src/graph.rs
@@ -39,7 +39,7 @@ impl From<usize> for BuildId {
 }
 
 /// A single file referenced as part of a build.
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct File {
     /// Canonical path to the file.
     pub name: String,
